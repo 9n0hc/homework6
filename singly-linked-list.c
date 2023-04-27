9cdd23457a94e6bbc7ba308e01f56fd8bfedb607
9n0hc <twosilverguns@naver.com>
2023-04-27 19:20:44 +0900

    Add singly-linked-list.c file

---
+++ singly-linked-list.c
@@ -0,0 +1,340 @@
+/*
+c언어로 구현한 singly linked list 프로그램.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+	int key;
+	struct Node* link;
+} listNode;
+
+typedef struct Head {
+	struct Node* first;
+}headNode;
+
+headNode* initialize(headNode* h);
+int freeList(headNode* h);
+
+int insertFirst(headNode* h, int key);
+int insertNode(headNode* h, int key);
+int insertLast(headNode* h, int key);
+
+int deleteFirst(headNode* h);
+int deleteNode(headNode* h, int key);
+int deleteLast(headNode* h);
+int invertList(headNode* h);
+
+void printList(headNode* h);
+
+/*
+사용자의 입력에 따른 함수 호출을 구현.
+사용자는 list를 초기화하고, node를 삽입, 삭제, 출력하고, 프로그램을 종료할 수 있음.
+*/
+int main()
+{
+    printf("[----- [이은총]  [2022041043] -----]\n");
+	char command;
+	int key;
+	headNode* headnode=NULL;
+
+	do{
+		printf("----------------------------------------------------------------\n");
+		printf("                     Singly Linked List                         \n");
+		printf("----------------------------------------------------------------\n");
+		printf(" Initialize    = z           Print         = p \n");
+		printf(" Insert Node   = i           Delete Node   = d \n");
+		printf(" Insert Last   = n           Delete Last   = e\n");
+		printf(" Insert First  = f           Delete First  = t\n");
+		printf(" Invert List   = r           Quit          = q\n");
+		printf("----------------------------------------------------------------\n");
+
+		printf("Command = ");
+		scanf(" %c", &command);
+
+		switch(command) {
+		case 'z': case 'Z':
+			headnode = initialize(headnode);
+			break;
+		case 'p': case 'P':
+			printList(headnode);
+			break;
+		case 'i': case 'I':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertNode(headnode, key);
+			break;
+		case 'd': case 'D':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			deleteNode(headnode, key);
+			break;
+		case 'n': case 'N':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertLast(headnode, key);
+			break;
+		case 'e': case 'E':
+			deleteLast(headnode);
+			break;
+		case 'f': case 'F':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertFirst(headnode, key);
+			break;
+		case 't': case 'T':
+			deleteFirst(headnode);
+			break;
+		case 'r': case 'R':
+			invertList(headnode);
+			break;
+		case 'q': case 'Q':
+			freeList(headnode);
+			break;
+		default:
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			break;
+		}
+
+	}while(command != 'q' && command != 'Q');
+
+	return 1;
+}
+
+/*
+headNode에 메모리 할당하고 첫 번째 pointer를 NULL로 설정.
+*/
+headNode* initialize(headNode* h) {
+
+	if(h != NULL)
+		freeList(h);
+
+	headNode* temp = (headNode*)malloc(sizeof(headNode));
+	temp->first = NULL;
+	return temp;
+}
+
+/*
+각 node와 headNode에 할당된 메모리를 free.
+*/
+int freeList(headNode* h){
+	listNode* p = h->first;
+
+	listNode* prev = NULL;
+	while(p != NULL) {
+		prev = p;
+		p = p->link;
+		free(prev);
+	}
+	free(h);
+	return 0;
+}
+
+/*
+sorted order 속 linked list의 주어진 key를 가진 새로운 node를 삽입.
+*/
+int insertNode(headNode* h, int key) {
+
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	node->key = key;
+	node->link = NULL;
+
+	if (h->first == NULL)
+	{
+		h->first = node;
+		return 0;
+	}
+
+	listNode* n = h->first;
+	listNode* trail = h->first;
+
+	while(n != NULL) {
+		if(n->key >= key) {
+			if(n == h->first) {
+				h->first = node;
+				node->link = n;
+			} else { 
+				node->link = n;
+				trail->link = node;
+			}
+			return 0;
+		}
+
+		trail = n;
+		n = n->link;
+	}
+
+	trail->link = node;
+	return 0;
+}
+
+/*
+linked list의 마지막에 지정된 key를 가진 새로운 node를 삽입.
+*/
+int insertLast(headNode* h, int key) {
+    listNode* node = (listNode*)malloc(sizeof(listNode));
+    node->key = key;
+    node->link = NULL;
+
+    if (h->first == NULL) {
+        h->first = node;
+        return 0;
+    }
+
+    listNode* n = h->first;
+    while (n->link != NULL) {
+        n = n->link;
+    }
+
+    n->link = node;
+    return 0;
+}
+
+/*
+linked list의 처음에 지정된 key를 가진 새로운 node를 삽입.
+*/
+int insertFirst(headNode* h, int key) {
+
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	node->key = key;
+
+	node->link = h->first;
+	h->first = node;
+
+	return 0;
+}
+
+/*
+linked list에서 지정된 key를 가진 node를 삭제.
+*/
+int deleteNode(headNode* h, int key) {
+
+	if (h->first == NULL)
+	{
+		printf("nothing to delete.\n");
+		return 0;
+	}
+
+	listNode* n = h->first;
+	listNode* trail = NULL;
+
+	while(n != NULL) {
+		if(n->key == key) {
+			if(n == h->first) {
+				h->first = n->link;
+			} else { 
+				trail->link = n->link;
+			}
+			free(n);
+			return 0;
+		}
+
+		trail = n;
+		n = n->link;
+	}
+
+	printf("cannot find the node for key = %d\n", key);
+	return 0;
+
+}
+
+/*
+linked list의 마지막 노드 삭제.
+*/
+int deleteLast(headNode* h) {
+
+	if (h->first == NULL)
+	{
+		printf("nothing to delete.\n");
+		return 0;
+	}
+
+	listNode* n = h->first;
+	listNode* trail = NULL;
+
+	if(n->link == NULL) {
+		h->first = NULL;
+		free(n);
+		return 0;
+	}
+
+	while(n->link != NULL) {
+		trail = n;
+		n = n->link;
+	}
+
+	trail->link = NULL;
+	free(n);
+
+	return 0;
+}
+
+/*
+linked list의 첫 번째 노드 삭제.
+*/
+int deleteFirst(headNode* h) {
+
+	if (h->first == NULL)
+	{
+		printf("nothing to delete.\n");
+		return 0;
+	}
+	listNode* n = h->first;
+
+	h->first = n->link;
+	free(n);
+
+	return 0;
+}
+
+/*
+linked list를 되돌림.
+*/
+int invertList(headNode* h) {
+
+
+	if(h->first == NULL) {
+		printf("nothing to invert...\n");
+		return 0;
+	}
+	listNode *n = h->first;
+	listNode *trail = NULL;
+	listNode *middle = NULL;
+
+	while(n != NULL){
+		trail = middle;
+		middle = n;
+		n = n->link;
+		middle->link = trail;
+	}
+
+	h->first = middle;
+
+	return 0;
+}
+
+/*
+linked list에 있는 모든 node의 key를 출력.
+*/
+void printList(headNode* h) {
+	int i = 0;
+	listNode* p;
+
+	printf("\n---PRINT\n");
+
+	if(h == NULL) {
+		printf("Nothing to print....\n");
+		return;
+	}
+
+	p = h->first;
+
+	while(p != NULL) {
+		printf("[ [%d]=%d ] ", i, p->key);
+		p = p->link;
+		i++;
+	}
+
+	printf("  items = %d\n", i);
+}
